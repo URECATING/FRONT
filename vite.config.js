bdadbc8b0065046a36d816fff02d3d1319612852
고경남 <rhrudska987@naver.com>
2024-09-24 11:11:05 +0900

    Feat: 댓글 기능 구 현

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,4 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@apis": path.resolve(__dirname, "./src/apis"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@constants": path.resolve(__dirname, "./src/constants"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@atoms": path.resolve(__dirname, "./src/atoms"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@types": path.resolve(__dirname, "./src/types"),
-    },
-  },
 });
